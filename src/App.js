d432462fa137e83b03e088fa53b3f267f95d15c2
RyzenMu <muruga13in2022@gmail.com>
2025-01-05 16:36:40 +0530

    added media query for mob, tab, and lap

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import kanyakumariPic from "./img/kanyakumari.png";
 function App() {
   return (
     <div className="App">
-      <img src={kanyakumariPic} alt="pic of kanyakumari" width={250} />
+      <img src={kanyakumariPic} alt="pic of kanyakumari" />
     </div>
   );
 }
