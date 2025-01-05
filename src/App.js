29e6d40033c305f2d8d7d44b5bc634b4b95c8e3b
RyzenMu <muruga13in2022@gmail.com>
2025-01-05 15:56:27 +0530

    kanyakumari pic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import kanyakumariPic from "./img/kanyakumari.png";
 
 function App() {
-  return <div className="App">hello react</div>;
+  return (
+    <div className="App">
+      <img src={kanyakumariPic} alt="pic of kanyakumari" width={250} />
+    </div>
+  );
 }
 
 export default App;
