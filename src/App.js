1dd7754c905d7d7cceb0da5c06e428bdf47f4bb9
RyzenMu <muruga13in2022@gmail.com>
2025-01-05 19:57:42 +0530

    project1 completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import "./App.css";
 import kanyakumariPic from "./img/kanyakumari.png";
+import thirunelveliPic from "./img/tirunelveli.png";
 
 function App() {
-  function handleClick() {
-    const imgEl = document.querySelector(".imgKanyakumari");
+  function handleClick(e) {
     const modalEl = document.querySelector(".modal");
-    imgEl.classList.toggle("zoom");
     modalEl.classList.toggle("show-modal");
+    console.log(e.target.dataset.location);
+    const clickedEl = e.target;
+    console.log(clickedEl);
+    // append-child
+    const modalImg = document.querySelector(".modal-img");
+    console.log(modalImg);
+    modalImg.appendChild(clickedEl);
   }
 
   return (
     <div className="App">
+      <div className="modal">
+        <div className="modal-img"></div>
+      </div>
+
       <img
         src={kanyakumariPic}
         alt="pic of kanyakumari"
         onClick={handleClick}
-        className="imgKanyakumari"
+        data-location="kanyakumari"
       />
-      <div className="modal"></div>
+
+      <img
+        src={thirunelveliPic}
+        alt="pic of tirunelveli"
+        onClick={handleClick}
+        data-location="tirunelveli"
+      />
     </div>
   );
 }
