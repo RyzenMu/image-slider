9ff74fdcd070cb105c0e50efade3bf00487ba4fc
RyzenMu <muruga13in2022@gmail.com>
2025-01-05 17:57:38 +0530

    modal added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import "./App.css";
 import kanyakumariPic from "./img/kanyakumari.png";
 
 function App() {
+  function handleClick() {
+    const imgEl = document.querySelector(".imgKanyakumari");
+    const modalEl = document.querySelector(".modal");
+    imgEl.classList.toggle("zoom");
+    modalEl.classList.toggle("show-modal");
+  }
+
   return (
     <div className="App">
-      <img src={kanyakumariPic} alt="pic of kanyakumari" />
+      <img
+        src={kanyakumariPic}
+        alt="pic of kanyakumari"
+        onClick={handleClick}
+        className="imgKanyakumari"
+      />
+      <div className="modal"></div>
     </div>
   );
 }
